92fcbaadecdcd5c0f27071c931ac8b6901000d73
Robert Anchondo <djangocharm2020@gmail.com>
2018-08-23 21:43:56 -0500

    First Commit
    Created the first Commit this has the Steps needed to create a Function in Python and using SMTP to send emails using your Google Gmail account without having to open Gmail instead you can open up a Python IDLE command and run it and send a message to another Email account.

---
+++ sendemail.py
@@ -0,0 +1,13 @@
+#Send Email
+import smtplib
+
+def sendEmail():
+    #create object in this case we User GMAIL , 587 is Gmail.
+    smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
+    #Encryption.
+    smtpObj.starttls()
+    #Time to login to your email, then enter your password.
+    smtpObj.login(' youremail@gmail.com', 'passwordhere')
+    #Now Create your Message remember to add your email, then receivers email, subject, message.
+    smtpObj.sendmail(' youremail@gmail.com', 'receiversemail@gmail.com',
+		 'Subject: How you been., Remember me,')
